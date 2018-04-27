8da086dd622eb6d4474fb088708d72f95faa6c78
unlimitbladeworks <13121347080@sina.cn>
2018-04-27 18:31:25 +0800

    添加栈接口

---
+++ Chapter2-Stacks-Queues/code/src/com/imooc/stacks/MainTest.java
@@ -0,0 +1,20 @@
+package com.imooc.stacks;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * @ClassName MainTest
+ * @Description 栈和队列的测试类
+ * @author suyu
+ * @Date 2018/4/27 14:28
+ * @version 1.0.0
+ */
+public class MainTest {
+
+    @Test
+    public void testArrayList(){
+    }
+}
