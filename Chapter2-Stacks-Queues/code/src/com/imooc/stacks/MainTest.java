1a1648ece105694360afc384f26404449892d21f
unlimitbladeworks <13121347080@sina.cn>
2018-05-01 22:38:52 +0800

    修改了github测试的中文乱码

---
+++ Chapter2-Stacks-Queues/code/src/com/imooc/stacks/MainTest.java
@@ -0,0 +1,28 @@
+package com.imooc.stacks;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * @author suyu
+ * @version 1.0.0
+ * @ClassName MainTest
+ * @Description 数组栈的测试类
+ * @Date 2018/4/27 14:28
+ */
+public class MainTest {
+
+    @Test
+    public void testArrayStack() {
+        ArrayStack<Integer> stack = new ArrayStack<>();
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+
+        stack.pop();
+        System.out.println(stack);
+    }
+}
