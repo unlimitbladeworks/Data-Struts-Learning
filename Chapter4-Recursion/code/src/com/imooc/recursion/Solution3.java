3d4e214f45fb883531a336b13a3caa6ea5000c12
unlimitbladeworks <13121347080@sina.cn>
2018-05-10 22:19:08 +0800

    添加了链表的递归删除测试用例

---
+++ Chapter4-Recursion/code/src/com/imooc/recursion/Solution3.java
@@ -0,0 +1,37 @@
+package com.imooc.recursion;
+
+public class Solution3 {
+
+    /**
+     * 递归实现leetCode上的203题
+     * 递归删除链表节点
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+
+        //如果传入节点为空,则返回空
+        if (head == null){
+            return null;
+        }
+
+        /**
+         * 递归自身函数,进行删除调用
+         * 当链表的值等于传入要删除的值,返回下一个
+         * 还是有点疑惑留给明天把
+         */
+        head.next = removeElements(head.next,val);
+        return head.val == val ? head.next : head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 6, 4, 5, 6};
+        ListNode listNode = new ListNode(arr);
+        System.out.println(listNode);
+
+        Solution3 solution3 = new Solution3();
+        solution3.removeElements(listNode, 6);
+        System.out.println(listNode);
+    }
+}
