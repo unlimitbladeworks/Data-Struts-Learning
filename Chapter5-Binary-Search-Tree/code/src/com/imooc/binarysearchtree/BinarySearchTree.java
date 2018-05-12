9b8301416c35d8b367a9539ecf9c5e20c689ad6d
unlimitbladeworks <13121347080@sina.cn>
2018-05-12 23:20:27 +0800

    添加了二分搜索树的插入数据方法

--- Chapter5-Binary-Search-Tree/code/src/com/imooc/binarysearchtree/BinarySearchTree.java
+++ Chapter5-Binary-Search-Tree/code/src/com/imooc/binarysearchtree/BinarySearchTree.java
@@ -47,18 +47,68 @@ public class BinarySearchTree<E extends Comparable<E>> {
 
     /**
      * 获取树的容量
+     *
      * @return 树容量
      */
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
     /**
      * 判断是否为空
+     *
      * @return
      */
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
+    }
+
+    /**
+     * 向二分搜索树中添加数据
+     *
+     * @param e 添加的数据
+     */
+    public void add(E e) {
+
+        //如果根节点是空,创建根节点,维护size容量增加
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    /**
+     * 向以node为根的二分搜索树中插入E,递归算法
+     *
+     * @param node
+     * @param e
+     */
+    private void add(Node node, E e) {
+
+        /**
+         * 插入的e与当前的e相等,不做处理
+         * 如果插入的e小于当前根节点的e,并且根节点的左孩子为空，则添加e到根节点的左孩子处
+         * 如果插入的e大于当前根节点的e,并且根节点的右孩子为空，则添加e到根节点的右孩子处
+         */
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+        //再次递归调用,调用这个私有的自身方法
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        } else {
+            add(node.right, e);
+        }
     }
 
 
