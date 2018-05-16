b2aa505bc335d69c13ad84071c2513fb4312f84e
unlimitbladeworks <13121347080@sina.cn>
2018-05-16 22:51:16 +0800

    添加了是删除二分搜索树的最小值和最大值以及测试案例

--- Chapter5-Binary-Search-Tree/code/src/com/imooc/binarysearchtree/BinarySearchTreeLevelUp.java
+++ Chapter5-Binary-Search-Tree/code/src/com/imooc/binarysearchtree/BinarySearchTreeLevelUp.java
@@ -252,6 +252,114 @@ public class BinarySearchTreeLevelUp<E extends Comparable<E>> {
         System.out.println(node.e);
     }
 
+    /**
+     * 寻找二分树的最小值
+     *
+     * @return
+     */
+    public E minimum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+
+        return minimum(root).e;
+    }
+
+    /**
+     * 返回以node为根的二分搜索树的最小节点
+     *
+     * @param node 节点
+     * @return
+     */
+    private Node minimum(Node node) {
+
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    /**
+     * 寻找二分树的最大值
+     *
+     * @return
+     */
+    public E maximum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        return maximum(root).e;
+    }
+    /**
+     * 返回以node为根的二分搜索树的最大节点
+     *
+     * @param node 节点
+     * @return
+     */
+    private Node maximum(Node node) {
+        if (node.right == null){
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+    /**
+     * 从二分搜索树中删除最小值所在的节点，返回最小值
+     * @return
+     */
+    public E removeMin(){
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    /**
+     * 删除以node为根的二分搜索树中的最小节点
+     * 返回删除节点后新的二分搜索树的根
+     * @param node
+     * @return
+     */
+    private Node removeMin(Node node) {
+        if (node.left == null){
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+
+    /**
+     * 从二分搜索树中删除最大值所在的节点，返回最大值
+     * @return
+     */
+    public E removeMax(){
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    /**
+     * 删除以node为根的二分搜索树中的最大节点
+     * 返回删除节点后新的二分搜索树的根
+     * @param node
+     * @return
+     */
+    private Node removeMax(Node node) {
+        if (node.right == null){
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+
+        node.right = removeMax(node.right);
+        return node;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- Chapter5-Binary-Search-Tree/code/src/com/imooc/binarysearchtree/MainTest.java
+++ Chapter5-Binary-Search-Tree/code/src/com/imooc/binarysearchtree/MainTest.java
@@ -2,6 +2,9 @@ package com.imooc.binarysearchtree;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * @author suyu
  * @version 1.0.0
@@ -46,8 +49,45 @@ public class MainTest {
         //∫Û–Ú±È¿˙
         bst.postOrder();
         System.out.println();
+    }
 
+    @Test
+    public void testRemoveMin() {
+        BinarySearchTreeLevelUp<Integer> bst = new BinarySearchTreeLevelUp<>();
+        Random random = new Random();
+        for (int i = 0; i < 1000; i++) {
+            bst.add(random.nextInt(1000));
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            list.add(bst.removeMin());
+        }
+        System.out.println(list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i - 1) > list.get(i)) {
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("removeMin test completed");
+    }
 
-
+    @Test
+    public void testRemoveMax() {
+        BinarySearchTreeLevelUp<Integer> bst = new BinarySearchTreeLevelUp<>();
+        Random random = new Random();
+        for (int i = 0; i < 1000; i++) {
+            bst.add(random.nextInt(1000));
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            list.add(bst.removeMax());
+        }
+        System.out.println(list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i - 1) < list.get(i)) {
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("removeMax test completed");
     }
 }
