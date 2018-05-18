333a7dea31dd8cc2c277172259e2d3f2b4b02d7f
unlimitbladeworks <13121347080@sina.cn>
2018-05-19 00:52:48 +0800

    add set

---
+++ Chapter6-Set-Map/code/src/com/imooc/set/Set.java
@@ -0,0 +1,44 @@
+package com.imooc.set;
+
+/**
+ * @author suyu
+ * @version 1.0.0
+ * @ClassName Set
+ * @Description 自定义集合的接口
+ * @Date 2018/5/18 22:58
+ */
+public interface Set<E> {
+
+    /**
+     * 添加元素
+     * @param e
+     * @return
+     */
+    E add(E e);
+
+    /**
+     * 删除元素
+     * @param e
+     */
+    void remove(E e);
+
+    /**
+     * 是否包含元素
+     * @param e
+     * @return
+     */
+    boolean contains(E e);
+
+    /**
+     * 获取集合大小
+     * @return
+     */
+    int getSize();
+
+    /**
+     * 是否为空
+     * @return
+     */
+    boolean isEmpty();
+
+}
