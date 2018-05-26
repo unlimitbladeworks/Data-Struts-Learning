17e5f6f6b2db26f37c57189d43e523e3ecb7290b
unlimitbladeworks <13121347080@sina.cn>
2018-05-26 11:59:47 +0800

    添加了BSTMap剩下的方法以及测试用例

--- Chapter6-Set-Map/code/src/com/imooc/map/BSTMap.java
+++ Chapter6-Set-Map/code/src/com/imooc/map/BSTMap.java
@@ -45,7 +45,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
 
     @Override
     public void add(K key, V value) {
-        //添加根节点k-v值
+        //添加二分搜索树添加元素：根节点k-v值
         root = add(root, key, value);
     }
 
@@ -58,37 +58,177 @@ public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
      * @return
      */
     private Node add(Node node, K key, V value) {
+        //如果节点为空,创建新的树节点
         if (node == null) {
             size++;
             return new Node(key, value);
         }
+        //传入的值与树节点做对比,小于递归左子树,大于递归右子树
         if (key.compareTo(node.key) < 0) {
-            add(node.left, key, value);
+            node.left = add(node.left, key, value);
         } else if (key.compareTo(node.key) > 0) {
-            add(node.right, key, value);
+            node.right = add(node.right, key, value);
+        } else { //key.compareTo(node.key) == 0
+            node.value = value;
         }
         return node;
     }
 
+
+    /**
+     * 返回以node为根的二分搜索树的最小值所在节点
+     *
+     * @param node node节点
+     * @return
+     */
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    /**
+     * 删除掉以node为根的二分搜索树中的最小节点
+     * 返回删除节点后新的二分搜索树的根
+     *
+     * @param node
+     * @return
+     */
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+
+    /**
+     * 删除对应key的值
+     *
+     * @param key
+     * @return
+     */
     @Override
     public V remove(K key) {
+        Node node = getNode(root, key);
+        if (node != null) {
+            root = remove(root, key);
+            return node.value;
+        }
         return null;
     }
 
+    /**
+     * 删除以node为根的二分搜索树中key的节点，递归算法
+     *
+     * @param node
+     * @param key
+     * @return 删除节点后的新节点
+     */
+    private Node remove(Node node, K key) {
+
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            return node;
+        } else { //key.compareTo(node.key)==0
+            //删除左子树为空的情况
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            //删除右子树为空的情况
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+
+            //删除左右子树都不为空的情况
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+        }
+    }
+
+
+    /**
+     * 返回以node为根节点的二分搜索树中，key所在的节点
+     * 递归遍历,根据传入的key值判断与节点的key值相比较,左右递归
+     *
+     * @param node 树节点
+     * @param key  key值
+     * @return node节点
+     */
+    private Node getNode(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        //如果key等于节点中的key,返回node,否则遍历找下去
+        if (key.equals(node.key)) {
+            return node;
+        } else if (key.compareTo(node.key) < 0) {
+            return getNode(node.left, key);
+        } else {
+            return getNode(node.right, key);
+        }
+    }
+
+    /**
+     * 根据getNode方法判断是否包含节点
+     *
+     * @param key
+     * @return
+     */
     @Override
     public boolean contains(K key) {
-        return false;
+        return getNode(root, key) != null;
     }
 
+    /**
+     * 获取对应键的值
+     *
+     * @param key
+     * @return
+     */
     @Override
     public V get(K key) {
-        return null;
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
     }
 
+    /**
+     * 根据key,更新对应的value值
+     *
+     * @param key
+     * @param newValue
+     */
     @Override
     public void set(K key, V newValue) {
-
+        Node node = getNode(root, key);
+        if (node == null) {
+            throw new IllegalArgumentException(key + "doesn't exist!");
+        }
+        node.value = newValue;
     }
+
 
     @Override
     public int getSize() {
--- Chapter6-Set-Map/code/src/com/imooc/map/MainTest.java
+++ Chapter6-Set-Map/code/src/com/imooc/map/MainTest.java
@@ -38,4 +38,28 @@ public class MainTest {
         }
 
     }
+
+    @Test
+    public void testBSTMap() {
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words1 = new ArrayList<>();
+
+
+        if (FileOperation.readFile("./Data-Struts-Learning/Chapter6-Set-Map/code/src/pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+
+            BSTMap<String, Integer> map = new BSTMap<>();
+            for (String word : words1) {
+                if (map.contains(word)) {
+                    map.set(word, map.get(word) + 1);
+                } else {
+                    map.add(word, 1);
+                }
+            }
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE : " + map.get("pride"));
+        }
+
+    }
 }
