e064ddbcff976155fdc206236014af11261d6723
unlimitbladeworks <13121347080@sina.cn>
2018-05-27 11:27:28 +0800

    添加了两个Map的测试时间用例

--- Chapter6-Set-Map/code/src/com/imooc/map/MainTest.java
+++ Chapter6-Set-Map/code/src/com/imooc/map/MainTest.java
@@ -62,4 +62,49 @@ public class MainTest {
         }
 
     }
+
+    /**
+     * 测试LinkedListMap 和 BSTMap 二者间的时间差测试用例
+     */
+    @Test
+    public void testTimeCompare() {
+        LinkedListMap<String, Integer> map = new LinkedListMap<>();
+        System.out.println("LinkedListMap:" + testMap(map) + "s");
+        BSTMap<String, Integer> map2 = new BSTMap<>();
+        System.out.println("BSTMap:" + testMap(map2) + "s");
+    }
+
+    /**
+     * 测试两个Map的时间
+     *
+     * @return
+     */
+    private double testMap(Map<String, Integer> map) {
+
+        long startTime = System.nanoTime();
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words1 = new ArrayList<>();
+
+
+        if (FileOperation.readFile("./Data-Struts-Learning/Chapter6-Set-Map/code/src/pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+
+            for (String word : words1) {
+                if (map.contains(word)) {
+                    map.set(word, map.get(word) + 1);
+                } else {
+                    map.add(word, 1);
+                }
+            }
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE : " + map.get("pride"));
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
 }
