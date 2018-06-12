c1abee9d67947d2073d55a4b2ff089d9a2c83230
unlimitbladeworks <13121347080@sina.cn>
2018-06-12 22:22:23 +0800

    modify numarrya2

--- Chapter8-Segment-Tree/code/src/segment/tree/NumArray2.java
+++ Chapter8-Segment-Tree/code/src/segment/tree/NumArray2.java
@@ -17,6 +17,8 @@ public class NumArray2 {
     /**
      * sum[i] 存储前i个元素和 sum[0]=0
      * sum[i] 存储nums[0...i-1]的和
+     * sum[0] ----> nums[0]
+     * sum[1] ----> nums[0,1] = -2 + 0 = -2
      */
     private static int[] sum;
 
@@ -33,7 +35,6 @@ public class NumArray2 {
         for (int i = 1; i < sum.length; i++) {
             sum[i] = sum[i - 1] + nums[i - 1];
         }
-
     }
 
     public static int sumRange(int i, int j) {
@@ -51,7 +52,7 @@ public class NumArray2 {
     public static void main(String[] args) {
         NumArray2 numArray2 = new NumArray2(new int[]{-2, 0, 3, -5, 2, -1});
         System.out.println(sumRange(0, 2));
-        update(2,2);
-        System.out.println(sumRange(0,2));
+        update(2, 2);
+        System.out.println(sumRange(0, 2));
     }
 }
